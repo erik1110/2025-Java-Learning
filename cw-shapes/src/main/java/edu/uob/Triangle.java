2c715d852e8063271e256f75eddef1a5bdc17e24
erik1110 <oceanuheart@gmail.com>
2025-01-20 15:57:48 +0000

    feat: add cw-shapes

---
+++ cw-shapes/src/main/java/edu/uob/Triangle.java
@@ -0,0 +1,23 @@
+package edu.uob;
+
+public class Triangle {
+  private int side1;
+  private int side2;
+  private int side3;
+  // TODO implement me!
+  public Triangle(int side1, int side2, int side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
+
+  // TODO implement me!
+  public double calculateArea() {
+    return 0;
+  }
+
+  // TODO implement me!
+  public int calculatePerimeterLength() {
+    return 0;
+  }
+}
